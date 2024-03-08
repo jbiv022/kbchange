ed93a511b62a0a53f464be65ba8452b5c7ffc537
jbiv022 <93056905+jbiv022@users.noreply.github.com>
2024-03-08 11:56:00 -0600

    Update change.py
    det to dvorak

--- change.py
+++ change.py
@@ -1,10 +1,6 @@
 import win32api
 
-# To switch to English (US) layout
-win32api.LoadKeyboardLayout('00000409', 1)
-
-# To switch to Arabic layout
-win32api.LoadKeyboardLayout('00000401', 1)
-
-# For Dvorak layout
+# Load the Dvorak keyboard layout
 win32api.LoadKeyboardLayout("00010409", 1)  # or "00020409"
+
+print("Keyboard layout changed to Dvorak.")
